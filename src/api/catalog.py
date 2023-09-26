0f58eda50d5eeecf500454badde76cf9b71bab77
Arctic1ce <davariartin@gmail.com>
2023-09-26 10:06:03 -0700

    testing first query

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 
+import sqlalchemy
+from src import database as db
+
 router = APIRouter()
 
 
@@ -11,11 +14,16 @@ def get_catalog():
 
     # Can return a max of 20 items.
 
+    with db.engine.begin() as connection:
+        result = connection.execute("SELECT * FROM global_inventory")
+        for row in result:
+            print(row[0][0])
+
     return [
             {
                 "sku": "RED_POTION_0",
                 "name": "red potion",
-                "quantity": 1,
+                "quantity": row[0][0],
                 "price": 50,
                 "potion_type": [100, 0, 0, 0],
             }
