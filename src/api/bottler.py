71105730b858fc549806ba19e25ed776cd169258
Arctic1ce <davariartin@gmail.com>
2023-10-02 18:57:37 -0700

    implemented green and blue potions for bottling

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -22,25 +22,45 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory]):
     print(potions_delivered)
 
     red_potions = 0
+    green_potions = 0
+    blue_potions = 0
     red_ml = 0
+    green_ml = 0
+    blue_ml = 0
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
         for row in result:
             red_potions = row[0]
+            green_potions = row[1]
+            blue_potions = row[2]
             red_ml = row[3]
+            green_ml = row[4]
+            blue_ml = row[5]
 
     list_red_ml = 0
+    list_green_ml = 0
+    list_blue_ml = 0
     list_red_potions = 0
+    list_green_potions = 0
+    list_blue_potions = 0
     for potion in potions_delivered:
         list_red_ml += (potion.potion_type[0] * potion.quantity)
-        list_red_potions += potion.quantity
+        list_green_ml += (potion.potion_type[1] * potion.quantity)
+        list_blue_ml += (potion.potion_type[2] * potion.quantity)
+        list_red_potions += (potion.potion_type[0] * potion.quantity) / 100
+        list_green_potions += (potion.potion_type[1] * potion.quantity) / 100
+        list_blue_potions += (potion.potion_type[2] * potion.quantity) / 100
 
-    ml = red_ml - list_red_ml
-    potions = red_potions + list_red_potions
+    red_ml = red_ml - list_red_ml
+    green_ml = green_ml - list_green_ml
+    blue_ml = blue_ml - list_blue_ml
+    red_potions = red_potions + list_red_potions
+    green_potions = green_potions + list_green_potions
+    blue_potions = blue_potions + list_blue_potions
 
     with db.engine.begin() as connection:
-        stmt = sqlalchemy.text("UPDATE global_inventory SET num_red_ml = :a, num_red_potions = :b")
-        result = connection.execute(stmt, {"a": ml, "b": potions})
+        stmt = sqlalchemy.text("UPDATE global_inventory SET num_red_ml = :a, num_green_ml = :b, num_blue_ml = :c, num_red_potions = :d, num_green_potions = :e, num_blue_potions = :f")
+        result = connection.execute(stmt, {"a": red_ml, "b": green_ml, "c": blue_ml, "d": red_potions, "e": green_potions, "f": blue_potions})
 
     return "OK"
 
@@ -58,22 +78,33 @@ def get_bottle_plan():
     # Initial logic: bottle all barrels into red potions.
 
     red_ml = 0
+    green_ml = 0
+    blue_ml = 0
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
         for row in result:
             red_ml = row[3]
+            green_ml = row[4]
+            blue_ml = row[5]
     
     # if there is at least 100ml of red
-    amount = 0
+    amounts = [0, 0, 0]
     if red_ml >= 100:
-        amount = red_ml // 100
+        amounts[0] = red_ml // 100
+    if green_ml >= 100:
+        amounts[1] = green_ml // 100
+    if blue_ml >= 100:
+        amounts[2] = blue_ml // 100
 
-    if amount > 0:
-        return [
-                {
-                    "potion_type": [100, 0, 0, 0],
-                    "quantity": amount,
-                }
-            ]
-    else:
-        return []
+    result = []
+    for i in range(len(amounts)):
+        if amounts[i] > 0:
+            type = [0, 0, 0, 0]
+            quantity = amounts[i]
+            type[i] = quantity * 100
+            result.append({
+                "potion_type": type,
+                "quantity": quantity
+            })
+            
+    return result
