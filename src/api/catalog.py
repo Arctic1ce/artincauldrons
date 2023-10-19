8065364bacc55501d8cf289f97620b6ca0411621
Arctic1ce <davariartin@gmail.com>
2023-10-18 20:56:47 -0700

    implemented using ledgers in catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -19,11 +19,15 @@ def get_catalog():
         result = connection.execute(sqlalchemy.text("SELECT * FROM potions"))
         result_dict = result.mappings().all()
         for row in result_dict:
-            if row["quantity"] > 0:
+            potion_type = row["potion_type"]
+            result = connection.execute(sqlalchemy.text("SELECT SUM(change) AS quantity FROM potions_ledger_entries WHERE potion_type = :a"), {"a": potion_type})
+            quantity = result.first()[0]
+            if (quantity == None): continue
+            if quantity > 0:
                 results.append({
                     "sku": row["item_sku"],
                     "name": row["item_name"],
-                    "quantity": row["quantity"],
+                    "quantity": quantity,
                     "price": row["item_price"],
                     "potion_type": row["potion_type"],
                 })
